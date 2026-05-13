a5352e1fa401dadb5169b2f92de19f7dc86b637c
Alex <oleksi.ovsiannikov@gmail.com>
2026-05-13 22:52:21 +0200

    feat: add post quotes endpoint

--- backend/pro_be.js
+++ backend/pro_be.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json());
+
 const quotes = [
   {
     quote: "Hello world",
@@ -16,11 +18,29 @@ const quotes = [
   },
 ];
 
+
+// GET quote
 app.get("/", (req, res) => {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
+
+  const randomIndex =
+    Math.floor(Math.random() * quotes.length);
 
   res.json(quotes[randomIndex]);
 });
+
+
+// POST quote
+app.post("/api/quotes", (req, res) => {
+
+  const newQuote = req.body;
+
+  quotes.push(newQuote);
+
+  console.log(quotes);
+
+  res.send("Quote added");
+});
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
