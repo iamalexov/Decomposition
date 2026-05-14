f621c61b2d0c2feb54f74479476438545ba91499
Alex <oleksi.ovsiannikov@gmail.com>
2026-05-14 23:11:18 +0200

    feat: add basic chat frontend structure

---
+++ sprint2-chat-app/assets/main.js
@@ -0,0 +1,8 @@
+const socket = io()
+const messages = document.querySelector('.messages')
+const form = document.querySelector('.form')
+const input = document.querySelector('.input')
+const name = document.querySelector('.name')
+
+
+const userName = prompt('your name;')
