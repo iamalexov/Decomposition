3bad9073281597c6fafb825da3fd0e69236b6223
Alex <oleksi.ovsiannikov@gmail.com>
2026-05-15 21:52:29 +0200

    feat: implement realtime chat with socket.io

--- sprint2-chat-app/assets/main.js
+++ sprint2-chat-app/assets/main.js
@@ -2,7 +2,26 @@ const socket = io()
 const messages = document.querySelector('.messages')
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
-const name = document.querySelector('.name')
+const nameBlock = document.querySelector('.name')
 
 
 const userName = prompt('your name;')
+nameBlock.innerHTML = `${userName}`
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if(input.value) {
+        socket.emit('chat message', {
+            message: input.value,
+            name: userName
+        })
+        input.value = ''
+    }
+})
+
+socket.on('chat message', (data) => {
+    const item = document.createElement('li')
+    item.innerHTML =`<span>${data.name}</span>: ${data.message}`
+    messages.appendChild(item)
+})
